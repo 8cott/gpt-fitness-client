7c3429f90d8723c7f81f93afa1b4ecfbdc98ccc7
Scott <109820423+8cott@users.noreply.github.com>
2023-12-01 15:32:55 -0500

    added icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar-left'>
-        <img className='logo' src='public/logo.png' /> 
+        <img className='logo' src='https://gpt-fitness-chi.vercel.app/public/logo.png' /> 
         <Link to="/" className='gpt-fitness'>GPT Fitness</Link></div>
       <div className='navbar-right'>
         {!isLoggedIn ? (
